0afcbb68725ddf331730405a76856676673b29a0
Ethan <ejr@HHIH34NK.lw.hs>
2020-01-22 09:38:33 -0800

    WebPage

--- 1-Basic-HTML/js/sketch.js
+++ 1-Basic-HTML/js/sketch.js
@@ -1,2 +1,15 @@
-function setup() {}
-function draw() {}
+function myFunction() {
+    document.getElementById("dropDown").classList.toggle("show");
+  }
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropBtn')) {
+      var dropdowns = document.getElementsByClassName("dropContent");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
